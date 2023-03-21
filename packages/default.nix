2c8038748b7720d1fc3947b66baf93edf45bd7e9
Carl Thomé <carlthome@gmail.com>
2023-03-21 22:13:46 +0100

    Have default `nix build` build all packages

--- packages/default.nix
+++ packages/default.nix
@@ -1,6 +1,16 @@
-{ pkgs ? import <nixpkgs> { } }: {
+{ pkgs ? import <nixpkgs> { } }: rec {
   jax = pkgs.callPackage ./jax { };
   pytorch = pkgs.callPackage ./pytorch { };
   sklearn = pkgs.callPackage ./sklearn { };
   tensorflow = pkgs.callPackage ./tensorflow { };
+  all = pkgs.symlinkJoin {
+    name = "all";
+    paths = [
+      jax
+      pytorch
+      sklearn
+      tensorflow
+    ];
+  };
+  default = all;
 }
