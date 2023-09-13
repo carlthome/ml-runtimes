ad55ffe5730feada42a02d907b7baa17d4e3de2b
Carl Thomé <carlthome@gmail.com>
2023-09-14 01:16:58 +0200

    Move pkgs.symlinkJoin call into function

--- packages/default.nix
+++ packages/default.nix
@@ -1,21 +1,12 @@
-{ pkgs ? import <nixpkgs> { } }: rec {
-  beam = pkgs.callPackage ./beam { };
-  jax = pkgs.callPackage ./jax { };
-  pytorch = pkgs.callPackage ./pytorch { };
-  sklearn = pkgs.callPackage ./sklearn { };
-  tensorflow = pkgs.callPackage ./tensorflow { };
-  all = pkgs.symlinkJoin {
-    name = "all";
-    paths = [
-      beam
-      jax
-      pytorch
-      sklearn
-      tensorflow
-    ];
-    meta = {
-      description = "Python environments for machine learning development";
-    };
+{ pkgs ? import <nixpkgs> { } }:
+let
+  environments = {
+    beam = pkgs.callPackage ./beam { };
+    jax = pkgs.callPackage ./jax { };
+    pytorch = pkgs.callPackage ./pytorch { };
+    sklearn = pkgs.callPackage ./sklearn { };
+    tensorflow = pkgs.callPackage ./tensorflow { };
   };
-  default = all;
-}
+  all = packages: pkgs.symlinkJoin { name = "all"; paths = builtins.attrValues packages; };
+in
+environments // { default = all environments; }
